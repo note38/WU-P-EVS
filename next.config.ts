9673fd584115bedacc806bd2c4d13ada3f488841
note38 <chardiegotis2003@gmail.com>
2025-09-02 23:45:44 +0800

    redeploy

--- next.config.ts
+++ next.config.ts
@@ -33,10 +33,6 @@ const nextConfig: NextConfig = {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable",
           },
-          {
-            key: "Accept-Encoding",
-            value: "br, gzip",
-          },
         ],
       },
     ];
